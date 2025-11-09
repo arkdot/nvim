6088c847539b4950aff02dfd3076bcb45c52bea0
arkdot <198597192+arkdot@users.noreply.github.com>
2025-11-09 12:26:55 +0100

    config for blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,30 @@
 return {
     "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "default",
+        },
+        completion = {
+            ghost_text = { enabled = true },
+            menu = {
+                draw = {
+                    columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" }, },
+                    components = {
+                        source_name = {
+                            width = { max = 30 },
+                            text = function(ctx)
+                                return "[" .. ctx.source_name .. "]"
+                            end,
+                            highlight = "BlinkCmpSource",
+                        }
+                    },
+                },
+            },
+        },
+        sources = {
+            default = { "lsp", "buffer", "path", "snippets", },
+        },
+        fuzzy = { implementation = "lua" },
+        signature = { enabled = true },
+    },
 }
