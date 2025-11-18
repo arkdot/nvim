456b6481d0c28602d13f463619162f890b6e2942
arkdot <198597192+arkdot@users.noreply.github.com>
2025-11-18 11:52:41 +0100

    harpoon: save_on_toggle

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,7 +4,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
-        harpoon:setup() -- required
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+            },
+        })
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
